da3e59fb01beb30326474d461e4fa1ee5a70cc2c
branchpanic <computersunified@gmail.com>
2019-06-05 15:52:25 -0400

    Add resolution check

--- src/main/java/me/branchpanic/mods/rtfm/gui/DocumentationScreen.java
+++ src/main/java/me/branchpanic/mods/rtfm/gui/DocumentationScreen.java
@@ -5,21 +5,22 @@ import me.branchpanic.mods.rtfm.api.DocEntry;
 import me.branchpanic.mods.rtfm.gui.ImmutableTheme;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.render.GuiLighting;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.Window;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.util.Identifier;
+import org.lwjgl.opengl.GL11;
 
 public class DocumentationScreen extends Screen {
     public static final Identifier BACKGROUND_TEXTURE = new Identifier("rtfm", "textures/gui/info.png");
 
-    public static final int BACKGROUND_WIDTH = 182;
-    public static final int BACKGROUND_HEIGHT = 144;
-
     public static final int CORE_BACKGROUND_WIDTH = 177;
     public static final int CORE_BACKGROUND_HEIGHT = 50;
 
-    public static final int BOX_TOP_LEFT_X = 59;
-    public static final int BOX_TOP_LEFT_Y = 5;
+    public static final int BOX_LEFT = 59;
+    public static final int BOX_TOP = 5;
 
     public static final int NINEPATCH_TOP_EDGE_START_X = CORE_BACKGROUND_WIDTH;
     public static final int NINEPATCH_TOP_EDGE_START_Y = 0;
@@ -55,6 +56,11 @@ public class DocumentationScreen extends Screen {
     private int left;
     private int top;
 
+    private int textLeft;
+    private int textTop;
+    private int textWidth;
+    private int textHeight;
+
     private float offsetY;
 
     public DocumentationScreen(DocEntry entry, ItemStack representation) {
@@ -78,14 +84,15 @@ public class DocumentationScreen extends Screen {
         left = (int) (HORIZONTAL_MARGIN_FACTOR * width);
         top = (int) (VERTICAL_MARGIN_FACTOR * height);
 
-        view = new MarkdownView(t, entry.node(), new StatefulTextRenderer(t, font, left + BOX_TOP_LEFT_X + 8, top + BOX_TOP_LEFT_Y + 8, 300));
+        textLeft = left + BOX_LEFT + 4;
+        textTop = top + BOX_TOP + 4;
+        textWidth = (width - left) - BOX_LEFT - 4;
+        textHeight = (height - top) - top - (2 * NINEPATCH_DECORATION_SIZE) - 8;
+
+        view = new MarkdownView(t, entry.node(), new StatefulTextRenderer(t, font, textLeft, textTop, textWidth));
     }
 
-    @Override
-    public void render(int mouseX, int mouseY, float partialTicks) {
-        renderBackground();
-        super.render(mouseX, mouseY, partialTicks);
-
+    public void drawTextBoxBackground() {
         int right = width - left;
         int bottom = height - top;
 
@@ -122,7 +129,24 @@ public class DocumentationScreen extends Screen {
         blit(right - NINEPATCH_DECORATION_SIZE, bottom - NINEPATCH_DECORATION_SIZE, NINEPATCH_BOTTOM_RIGHT_U, NINEPATCH_BOTTOM_RIGHT_V, NINEPATCH_DECORATION_SIZE, NINEPATCH_DECORATION_SIZE);
         blit(left + NINEPATCH_LEFT_EDGE_START_X, bottom - NINEPATCH_DECORATION_SIZE, NINEPATCH_BOTTOM_LEFT_U, NINEPATCH_BOTTOM_LEFT_V, NINEPATCH_DECORATION_SIZE, NINEPATCH_DECORATION_SIZE);
 
-        fill(left + BOX_TOP_LEFT_X, top + BOX_TOP_LEFT_Y, right - NINEPATCH_DECORATION_SIZE, bottom - NINEPATCH_DECORATION_SIZE, NINEPATCH_FILL_COLOR);
+        fill(left + BOX_LEFT, top + BOX_TOP, right - NINEPATCH_DECORATION_SIZE, bottom - NINEPATCH_DECORATION_SIZE, NINEPATCH_FILL_COLOR);
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        renderBackground();
+        super.render(mouseX, mouseY, partialTicks);
+
+        if (width < CORE_BACKGROUND_WIDTH || height < CORE_BACKGROUND_HEIGHT) {
+            String sizeMessage = I18n.translate("ui.rtfm.too_small");
+            int size = font.getStringWidth(sizeMessage);
+            font.draw(sizeMessage, (width - size) / 2f, (height - font.fontHeight) / 2f, 0xFFFFFFFF);
+            return;
+        }
+
+        drawTextBoxBackground();
+
+        GuiLighting.enable();
 
         GlStateManager.pushMatrix();
         GlStateManager.translatef(left + 8f, top + 8f, 0f);
@@ -130,15 +154,35 @@ public class DocumentationScreen extends Screen {
         itemRenderer.renderGuiItem(representation, 0, 0);
         GlStateManager.popMatrix();
 
+        GuiLighting.disable();
+
+        Window window = MinecraftClient.getInstance().window;
+
+        int windowWidth = window.getWidth();
+        int windowHeight = window.getHeight();
+        float scaledWidth = window.getScaledWidth();
+        float scaledHeight = window.getScaledHeight();
+
+        int scissorX = (int) (windowWidth * (textLeft / scaledWidth));
+        int scissorY = (int) (windowHeight * (textTop / scaledHeight));
+        int width = (int) (windowWidth * (textWidth / scaledWidth));
+        int height = (int) (windowHeight * (textHeight / scaledHeight));
+
+        GL11.glScissor(scissorX, windowHeight - height - scissorY, width, height);
+
+        GL11.glEnable(GL11.GL_SCISSOR_TEST);
+
         GlStateManager.pushMatrix();
         GlStateManager.translatef(0, offsetY, 0);
         view.render(mouseX, mouseY, partialTicks);
         GlStateManager.popMatrix();
+
+        GL11.glDisable(GL11.GL_SCISSOR_TEST);
     }
 
     @Override
     public boolean mouseScrolled(double double_1, double double_2, double amount) {
-        offsetY += 4 * amount;
+        offsetY = (int) Math.min(offsetY + 4 * amount, 0);
         return true;
     }
 
